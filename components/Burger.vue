0e1c7a9002e7635402a4b28eb836c289191530b0
bourzeib <baptiste.bourzeix.external@amersports.com>
2021-12-26 04:34:47 +0100

    Set website structure and wireframe

---
+++ components/Burger.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="burger-menu fixed z-30 top-12 right-12 w-8 h-8 cursor-pointer" :class="{ 'mt-2': menuOpened }"
+       @click="toggleMenu">
+    <div
+        class="block h-0.5 w-full bg-white transform transition-all"
+        :class="{ 'rotate-45 mb-0 delay-300': menuOpened, 'mb-2': !menuOpened }"
+    ></div>
+    <div
+        class="h-0.5 w-9/12 bg-white mb-2 transition-all delay-300"
+        :class="{ 'opacity-0 h-0 mb-0 hidden -mr-1': menuOpened, 'block': !menuOpened, }"
+    ></div>
+    <div
+        class="block h-0.5 bg-white transform transition-all"
+        :class="{ '-rotate-45 mb-0 w-full delay-300': menuOpened, 'w-10/12': !menuOpened,  }"
+    ></div>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  computed: {
+    menuOpened () {
+      return this.$store.state.menuOpened
+    }
+  },
+  methods: {
+    ...mapMutations({
+      toggleMenu: "toggleMenu"
+    })
+  }
+}
+</script>
+
+<style>
+.burger-menu {
+  &:hover {
+    > div {
+      width: 100%;
+    }
+  }
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="p-12">
+    <NuxtLink class="inline-block" to="/">
+      <Logo />
+    </NuxtLink>
+    <Navigation />
+  </div>
+</template>
+
+<script>
+import Logo from "~/assets/svg/logo.svg?inline";
+import Navigation from "~/components/Navigation";
+
+export default {
+  head: {
+    bodyAttrs: {
+      class: 'bg-indigo-900',
+    }
+  },
+  components: { Logo, Navigation },
+}
+</script>
